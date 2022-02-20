837e90b3550440c691e0772a447dba1f8b791ef0
shabdmaharaj <shabd@citiqprepaid.co.za>
2022-02-20 16:18:23 +0200

    added recipe and tag modle with tests

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -13,7 +13,7 @@ class AdminSiteTests(TestCase):
         )
         self.client.force_login(self.admin_user)
         self.user = get_user_model().objects.create_user(
-            email='shabd@gmail.com',
+            email='shabd1@gmail.com',
             password='password123',
             name='Test user full name'
         )
